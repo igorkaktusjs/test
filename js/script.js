21157bfc4514b1c9494f24261d64f5dcf6217947
igor <igorkaktus@gmail.com>
2022-05-19 10:12:15 +0100

    19 may

--- js/script.js
+++ js/script.js
@@ -1,81 +1,34 @@
-    
-    
 
-    // Место для первой задачи
-function firstTask() {
-    for (let i=5; i<= 10; i++){
-    console.log(i);
-}
-    
-}
+function getMathResult(num, times) {
+    if(typeof(times)== 'number'){
 
-// Место для второй задачи
-function secondTask() {
-    // Пишем решение вот тут
-    for (let i=20; i>9;){
-    console.log(i);
-    --i;
-    if(i==12)
-    {
-        break;
-    }
-}
-    
-}
-
-// Место для третьей задачи
-function thirdTask() {
-    // Пишем решение вот тут
-    for (let i=2; i<12;++i){
-    console.log(i);
-    ++i;
-    }
-    
-}
-
-// Место для четвертой задачи
-
-// Цикл, который нужно переписать:
-
- for (let i = 2; i <= 16; i++) {
-   if (i % 2 === 0) {
-       continue;
-     } else {
-        console.log(i);
-     }
- }
-
-function fourthTask() {
-    // Пишем решение вот тут
-    let i = 2;
-
-    while( i>16 ) {
-        console.log(i);
-        i++;
-    }
-
-    while ( i<= 16){
-        i++;
-        if(i % 2 === 0)
-        {
-            
-        console.log(i);
+        let str = '';
         
-        continue;
+        for(let i = 1; i <= times ; ++i){
+            if (i === times){
+                let sum = num * i;
+            console.log (sum);
+            }else{
+                let sum = num * i;
+                console.log (sum+`---`);
+            }
+            
+        }
+    }else{
+        console.log(num);
+        return num;
     }
-    
-    }
-    
 }
+ getMathResult(5,7);
 
-// Место для пятой задачи
+ let str = '';
 
-function fifthTask() {
-    const arrayOfNumbers = [];
-
-    // Пишем решение вот тут
-    
-    
-    // Не трогаем
-    return arrayOfNumbers;
-}
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`; 
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
